4814f1d948b53bb572639e500ae21168ab4339b8
Antonio <tonymike20@hotmail.com>
2018-02-03 19:52:29 -0600

    AngularFire2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { FIREBASE_CONFIG } from './firebase.credentials';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { MyApp } from './app.component';
 
@@ -12,7 +15,9 @@ import { MyApp } from './app.component';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCTtvXik9XljM5jRFwHyHbpbTFVMtXixRc",
+    authDomain: "cokysdaycare-92cfa.firebaseapp.com",
+    databaseURL: "https://cokysdaycare-92cfa.firebaseio.com",
+    projectId: "cokysdaycare-92cfa",
+    storageBucket: "cokysdaycare-92cfa.appspot.com",
+    messagingSenderId: "325568766673"
+}
