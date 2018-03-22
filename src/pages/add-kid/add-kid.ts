6f6a6cdf2264bfac87022020b22a17d35edd1d5a
Antonio <tonymike20@hotmail.com>
2018-03-21 20:20:07 -0500

    probando 2do metodo para cargar imafenes a firebase

--- src/pages/add-kid/add-kid.ts
+++ src/pages/add-kid/add-kid.ts
@@ -6,7 +6,8 @@ import { Kid } from "../../models/kid/kid.model";
 import { KidService } from "../../services/kid/kid.service";
 import { ToastService } from "../../services/toast/toast.service";
 import { Camera, CameraOptions } from '@ionic-native/camera';
-import { CargaArchivoProvider } from "../../providers/carga-archivo/carga-archivo";
+import { storage, initializeApp } from 'firebase';
+// import { CargaArchivoProvider } from "../../providers/carga-archivo/carga-archivo";
 
 @IonicPage()
 @Component({
@@ -27,28 +28,41 @@ export class AddKidPage {
               public navParams: NavParams,
               private kids: KidService,
               private toast: ToastService,
-              private camara: Camera,
-              public _cap: CargaArchivoProvider) {}
+              private camara: Camera
+              // public _cap: CargaArchivoProvider
+             ) {}
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddKidPage');
   }
 
-  mostrar_camara() {
+  async takePhoto() {
+   try {
     const options: CameraOptions = {
       quality: 50,
       destinationType: this.camara.DestinationType.DATA_URL,
       encodingType: this.camara.EncodingType.JPEG,
       mediaType: this.camara.MediaType.PICTURE
     }
+    const result = await this.camara.getPicture(options);
 
-    this.camara.getPicture(options).then((imageData) => {
-      this.imagenPreview = 'data:image/jpeg;base64,' + imageData;
-      this.imagen64 = imageData;
-    }, (err) => {
-      // Handle error
-      console.log( "ERROR EN CAMARA", JSON.stringify(err) );
-    });
+    const image = `data:image/jpeg;base64,${result}`;
+
+    const pictures = storage().ref('pictures');
+    pictures.putString(image, 'data_url');
+  }
+  catch (e) {
+    console.error(e);
+
+  }
+
+    // this.camara.getPicture(options).then((imageData) => {
+    //   this.imagenPreview = 'data:image/jpeg;base64,' + imageData;
+    //   this.imagen64 = imageData;
+    // }, (err) => {
+    //   // Handle error
+    //   console.log( "ERROR EN CAMARA", JSON.stringify(err) );
+    // });
   }
 
   addKid(kid: Kid) {
@@ -61,14 +75,14 @@ export class AddKidPage {
     });
   }
 
-  crear_post(){
-
-    let archivo = {
-      img: this.imagen64,
-      titulo: this.titulo
-    }
-
-    this._cap.cargar_imagen_firebase(archivo);
-  }
+  // crear_post(){
+  //
+  //   let archivo = {
+  //     img: this.imagen64,
+  //     titulo: this.titulo
+  //   }
+  //
+  //   this._cap.cargar_imagen_firebase(archivo);
+  // }
 
 }
