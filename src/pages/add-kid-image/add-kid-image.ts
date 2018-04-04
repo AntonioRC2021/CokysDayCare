dc8c245dc343cd8e26850e9747c73fb0556c0558
Antonio <tonymike20@hotmail.com>
2018-04-04 04:13:16 -0500

    imageKey funcionando

--- src/pages/add-kid-image/add-kid-image.ts
+++ src/pages/add-kid-image/add-kid-image.ts
@@ -52,7 +52,7 @@ export class AddKidImagePage {
 
   const image = `data:image/jpeg;base64,${result}`;
 
-  const pictures = storage().ref('pictures/myPhoto');
+  const pictures = storage().ref('pictures');
   return pictures.putString(image, 'data_url')
   .then ( ref => {
           this.toast.show(`${ref.downloadURL} added!`);
@@ -63,88 +63,11 @@ export class AddKidImagePage {
          key: this.kid.key,
          imageKey: ref.downloadURL
        })
-  // .then(
-  //   this.kids.addImage(image)
-  // )
-  // .then(ref => {
-  //     this.kids.editKid({
-  //              lastName: this.kid.lastName,
-  //              name: this.kid.name,
-  //              parentId: this.kid.parentId,
-  //              secondParentId: this.kid.secondParentId,
-  //              imageKey: ref.
-  //     })
-  // })
 
 }) }
     catch (e) {
       console.error(e)
     }
   }
-
-
-  // selectPhoto() {
-  //   let options:ImagePickerOptions = {
-  //     quality: 70,
-  //     outputType: 1,
-  //     maximumImagesCount: 1
-  //   }
-  //
-  //   this.imagePicker.getPictures(options).then((results) => {
-  //     for (var i = 0; i < results.length; i++) {
-  //       // console.log('Image URI: ' + results[i]);
-  //       this.imagenPreview = 'data:image/jpeg;base64,' + results[i];
-  //     }
-  //   }, (err) => {
-  //     console.log( "ERROR en selector", JSON.stringify(err) );
-  //
-  //   });
-  // }
-
-
-
-
-
-
-
-
-
-
-//   mostrar_camara() {
-//   const options: CameraOptions = {
-//     quality: 50,
-//     destinationType: this.camara.DestinationType.DATA_URL,
-//     encodingType: this.camara.EncodingType.JPEG,
-//     mediaType: this.camara.MediaType.PICTURE
-//   }
-//
-//   this.camara.getPicture(options).then((imageData) => {
-//     this.imagenPreview = 'data:image/jpeg;base64,' + imageData;
-//     this.imagen64 = imageData;
-//   }, (err) => {
-//     // Handle error
-//     console.log( "ERROR EN CAMARA", JSON.stringify(err) );
-//   });
-// }
-//
-//
-//
-// crear_post(){
-//
-//      let archivo = {
-//        img: this.imagen64
-//      }
-//
-//      this.imageService.cargar_imagen_firebase(archivo).then(response => {
-//        this.kids.editKid({
-//          lastName: this.kid.lastName,
-//          name: this.kid.name,
-//          imageKey: "response"
-//        }).then(ref => {
-//          console.log("el nino se actualizo ", ref)
-//        })
-//      });
-//
-//    }
 
 }
