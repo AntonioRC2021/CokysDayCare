5e918b7bad59057771e248ba9582a1758c8f4f0d
Antonio <tonymike20@hotmail.com>
2018-04-03 20:03:05 -0500

    toast modificado

--- src/pages/add-kid-image/add-kid-image.ts
+++ src/pages/add-kid-image/add-kid-image.ts
@@ -55,7 +55,7 @@ export class AddKidImagePage {
   const pictures = storage().ref('pictures/myPhoto');
   pictures.putString(image, 'data_url')
   .then(ref => {
-      this.toast.show(`${JSON.stringify(ref)} added!`);
+      this.toast.show("tony:" + ref);
   });
 
   }
